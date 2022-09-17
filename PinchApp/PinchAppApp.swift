7fdc5452d706f42e926d00f5f9324123342f4a22
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-18 00:03:26 +0530

    Initial Commit

---
+++ PinchApp/PinchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchAppApp.swift
+//  PinchApp
+//
+//  Created by Usha Sai Chintha on 18/09/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
